bcea97b891912a1c7aa94fd71bdb9d5a111fd45b
Borhan Uddin <borhan.ud99@gmail.com>
2024-09-09 17:15:12 +0600

    Few changes added

--- src/main/java/com/demo/classroom/ClassroomApplication.java
+++ src/main/java/com/demo/classroom/ClassroomApplication.java
@@ -3,10 +3,19 @@ package com.demo.classroom;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class ClassroomApplication {
 
 	public static void main(String[] args) {
+		
+		Dotenv dotenv = Dotenv.load();
+        System.setProperty("spring.datasource.url", dotenv.get("SPRING_DATASOURCE_URL"));
+        System.setProperty("spring.datasource.username", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+        System.setProperty("spring.datasource.password", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+
+
 		SpringApplication.run(ClassroomApplication.class, args);
 	}
 
